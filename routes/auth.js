e078bcaaab4db21b8f34c272f948f4da6c45980d
haidousm <haidous.m@gmail.com>
2021-06-21 22:14:27 +0300

    implement logging in endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcrypt");
 const router = require("express").Router();
 const User = require("../models/User");
 
+// POST /api/auth/register
+// creates user
 router.post("/register", async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(16);
@@ -18,7 +20,29 @@ router.post("/register", async (req, res) => {
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (err) {
-        console.error(err);
+        res.status(500).json(err.message);
+    }
+});
+
+// POST /api/auth/login
+// login user
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email,
+        });
+        !user && res.status(404).json({ message: "User not found." });
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        !validPassword &&
+            res.status(400).json({ message: "Wrong email or password" });
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err.message);
     }
 });
 
